75d13bcd0dce91c4dd8cbafbea6508b694c51158
Maxwell Pray <synthead@gmail.com>
2015-02-16 19:15:28 -0800

    Add AnalogInputPin to gpio_controller.py.

--- gpio_controller.py
+++ gpio_controller.py
@@ -41,3 +41,32 @@ class YM2420(object):
     self.A0.write(1)
     self.CS.write(0)
     self.CS.write(1)
+
+
+class AnalogInputPin(object):
+  def __init__(
+      self, analog_pin, min_value=0, max_value=1019, sample_size=10):
+    self.analog_pin = analog_pin
+    self.min_value = min_value
+    self.max_value = max_value
+    self.sample_size = sample_size
+    self.values = [self.read_raw()] * self.sample_size
+
+  def read_raw(self):
+    return wiringpi2.analogRead(self.analog_pin)
+
+  def read(self):
+    self.values.pop(0)
+    self.values.append(self.read_raw())
+    return sum(self.values) / self.sample_size
+
+  def read_percent(self):
+    value = self.read()
+
+    if value <= self.min_value:
+      return 0
+    elif value >= self.max_value:
+      return 100
+    else:
+      return (
+          (value - self.min_value) / (self.max_value - self.min_value)) * 100
